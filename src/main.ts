40953554c8d2635c0ac2aeb5cab72be25ddc221d
Oleg Pustovit <ursus95@gmail.com>
2019-10-31 21:16:35 +0200

    Added framebuffer

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { CanvasOut } from "./canvas-out"
 let canvasGl = new WebGLQuad(<HTMLCanvasElement>document.getElementById('canvasgl'))
 let canvas2d = new CanvasOut(<HTMLCanvasElement>document.getElementById('canvas2d'))
 
-canvasGl.draw()
 canvas2d.drawText("Help")
 
 canvas2d.canvas.style.display = "none"
 canvasGl.canvas.style.display = "block"
+var pixel = canvas2d.ctx.getImageData(0, 0, canvas2d.canvas.width, canvas2d.canvas.height)
+// argb8
+canvasGl.uploadImage(pixel.data, pixel.width, pixel.height)
+canvasGl.draw()
--- src/webgl-quad.ts
+++ src/webgl-quad.ts
@@ -1,49 +1,25 @@
-export class WebGLQuad {
-  canvas: HTMLCanvasElement
-  gl: WebGLRenderingContext | null = null
-  program: WebGLProgram | null = null
+class ShaderProgram {
+  gl: WebGLRenderingContext
+  program: WebGLProgram
+  constructor (gl: WebGLRenderingContext, vertSource: string, fragSource: string) {
+    this.gl = gl
 
-  vertexBuffer: WebGLBuffer | null = null
-  vertexPositionAttribute: number = 0
+    let vert = <WebGLShader>this.compileShader(
+      vertSource, gl.VERTEX_SHADER)
+    let frag = <WebGLShader>this.compileShader(
+     fragSource, gl.FRAGMENT_SHADER)
 
-  constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas
-    this.gl = <WebGLRenderingContext>canvas.getContext('webgl')
-    if (!this.gl) {
-      console.log('WebGL is not supported on your browser')
+    this.program = <WebGLProgram>gl.createProgram();
+    gl.attachShader(this.program, vert)
+    gl.attachShader(this.program, frag)
+    gl.linkProgram(this.program)
+
+    if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+      console.log('Shader program is not linked: ' + gl.getProgramInfoLog(this.program))
+      return
     }
-
-    if (this.gl) {
-      this.initShaders()
-      this.initVertexBuffer([
-        0, 0,
-        0, 1,
-        1, 0,
-        1, 0,
-        0, 1,
-        1, 1,
-      ])
-      this.draw()
-    }
-  }
-
-  initVertexBuffer(v: number[]) {
-    let gl = <WebGLRenderingContext>this.gl
-    this.vertexBuffer = gl.createBuffer()
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(v), gl.STATIC_DRAW)
-  }
-
-  draw() {
-    let gl = <WebGLRenderingContext>this.gl
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-    const bufferSize = 6
-    gl.uniform2f(gl.getUniformLocation(<WebGLProgram>this.program, 'resolution'),
-      gl.canvas.width, gl.canvas.height)
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
-    gl.vertexAttribPointer(this.vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0)
-    gl.drawArrays(gl.TRIANGLES, 0, bufferSize)
+    gl.useProgram(this.program)
+    gl.useProgram(null)
   }
 
   compileShader(src: string, type: number) {
@@ -62,26 +38,139 @@ export class WebGLQuad {
     }
     return shader
   }
+}
 
-  initShaders() {
-    let gl = <WebGLRenderingContext>this.gl
-    let vert = <WebGLShader>this.compileShader(
-      (<HTMLElement>document.getElementById('drawImage-vertex-shader')).innerText, gl.VERTEX_SHADER)
-    let frag = <WebGLShader>this.compileShader(
-      (<HTMLElement>document.getElementById('drawImage-fragment-shader')).innerText, gl.FRAGMENT_SHADER)
+export class WebGLQuad {
+  canvas: HTMLCanvasElement
+  gl: WebGLRenderingContext | null = null
 
-    this.program = <WebGLProgram>gl.createProgram();
-    gl.attachShader(this.program, vert)
-    gl.attachShader(this.program, frag)
-    gl.linkProgram(this.program)
+  vertexBuffer: WebGLBuffer | null = null
 
-    if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
-      console.log('Shader program is not linked: ' + gl.getProgramInfoLog(this.program))
-      return
+  tex: WebGLTexture | null = null
+  framebufferTex: WebGLTexture | null = null
+
+  shaderFramebuffer: ShaderProgram
+  shaderMain: ShaderProgram
+
+  fb: WebGLFramebuffer | null = null
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas
+    this.gl = <WebGLRenderingContext>canvas.getContext('webgl')
+    if (!this.gl) {
+      throw('WebGL is not supported on your browser')
     }
-    gl.useProgram(this.program)
 
-    this.vertexPositionAttribute = gl.getAttribLocation(this.program, 'pos')
-    gl.enableVertexAttribArray(this.vertexPositionAttribute)
+    this.shaderFramebuffer = new ShaderProgram(this.gl, 
+      (<HTMLElement>document.getElementById('drawImage-vertex-shader')).innerText, 
+      (<HTMLElement>document.getElementById('drawImage-fragment-shader')).innerText)
+
+    this.shaderMain = new ShaderProgram(this.gl, 
+      (<HTMLElement>document.getElementById('drawImage-vertex-shader')).innerText, 
+      (<HTMLElement>document.getElementById('drawImage-fragfbo-shader')).innerText)
+
+      this.initFramebuffer()
+      this.initVertexBuffer([
+        0, 0,
+        0, 1,
+        1, 0,
+        1, 0,
+        0, 1,
+        1, 1,
+      ])
+  }
+
+  initFramebuffer() {
+    let gl = <WebGLRenderingContext>this.gl
+    this.framebufferTex = gl.createTexture()
+    let targetTexture = this.framebufferTex
+    gl.bindTexture(gl.TEXTURE_2D, targetTexture)
+    {
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA,
+        this.canvas.width, this.canvas.height, 0,
+                    gl.RGBA, gl.UNSIGNED_BYTE, null)
+    
+      // set the filtering so we don't need mips
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    }
+
+    this.fb = gl.createFramebuffer()
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.fb)
+    
+    // attach the texture as the first color attachment
+    const attachmentPoint = gl.COLOR_ATTACHMENT0
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, targetTexture, 0)
+  }
+
+  initVertexBuffer(v: number[]) {
+    let gl = <WebGLRenderingContext>this.gl
+    this.vertexBuffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(v), gl.STATIC_DRAW)
+  }
+
+  draw() {
+    let gl = <WebGLRenderingContext>this.gl
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+
+    const bufferSize = 6
+
+    // render to our targetTexture by binding the framebuffer
+    gl.bindFramebuffer(gl.FRAMEBUFFER, this.fb)
+
+    let program = <WebGLProgram>this.shaderFramebuffer.program
+    gl.useProgram(program)
+    gl.bindTexture(gl.TEXTURE_2D, this.tex)
+    gl.uniform1i(gl.getUniformLocation(program, 'texture'), 0)
+
+    gl.uniform2f(gl.getUniformLocation(program, 'resolution'),
+      gl.canvas.width, gl.canvas.height)
+
+    let attr = 0
+    let that = this
+    let drawThis = function() {
+      attr = gl.getAttribLocation(program, 'pos')
+      gl.bindBuffer(gl.ARRAY_BUFFER, that.vertexBuffer)
+      gl.enableVertexAttribArray(attr)
+      gl.vertexAttribPointer(attr, 2, gl.FLOAT, false, 0, 0)
+      gl.drawArrays(gl.TRIANGLES, 0, bufferSize)
+      gl.bindBuffer(gl.ARRAY_BUFFER, null)
+    }
+    drawThis()
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+    program = <WebGLProgram>this.shaderMain.program
+    gl.useProgram(program)
+    gl.bindTexture(gl.TEXTURE_2D, this.framebufferTex)
+    gl.uniform1i(gl.getUniformLocation(program, 'texture'), 0)
+    drawThis()
+
+    gl.drawArrays(gl.TRIANGLES, 0, bufferSize)
+
+    gl.useProgram(null)
+  }
+
+
+
+  uploadImage(data: Uint8ClampedArray, width: number, height: number) {
+    let gl = <WebGLRenderingContext>this.gl
+    if (!this.tex) {
+      this.tex = gl.createTexture()
+    } 
+    gl.bindTexture(gl.TEXTURE_2D, this.tex)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    var array = new Uint8Array(data.buffer)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, array)
   }
 }
+
+// draw to framebuffer
+// draw to render view
+// y rows
+// each framebuffer iteration read back
+// get pixel row
